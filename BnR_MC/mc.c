be78ffe0c51475ec24199def877c9ce64b3bf623
Cheery Guo <cheery.guo@gmail.com>
2014-01-08 11:44:40 +0800

    replace tab to whitespace for mc.c

--- BnR_MC/mc.c
+++ BnR_MC/mc.c
@@ -13,7 +13,7 @@ FILE NAME    : $RCSfile$
 
 AUTHOR       : $Author$
 
-DESCRIPTION  :  
+DESCRIPTION  :
 
 Revision     : $Revision$
 
@@ -66,21 +66,21 @@ void get_err_str(int err_id, char* err_str);
 
 
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_move_absolute
  *
- * description: 
- *  This function commands a controlled motion at a specific absolute position. 
- *  If command is executed successfully, 0 is returned; otherwise, corresponding 
+ * description:
+ *  This function commands a controlled motion at a specific absolute position.
+ *  If command is executed successfully, 0 is returned; otherwise, corresponding
  *  error number is returned.
  *
  * input:
- *  Position: Target position for the motion [Units]. 
+ *  Position: Target position for the motion [Units].
  *  velocity: Value of maximum velocity [Units/sec] (not necessarily reached).
- *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily 
- *					reached). 
- *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily 
- *					reached).   
+ *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily
+ *					reached).
+ *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily
+ *					reached).
  *  Direction: Movement direction:
  *					mcPOSITIVE_DIR ... 0
  *					mcNEGATIVE_DIR ... 1
@@ -90,18 +90,18 @@ void get_err_str(int err_id, char* err_str);
  *					mcAUTOMAT_POS .. 100
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 
-int mc_move_absolute(int Position, int velocity, 
+int mc_move_absolute(int Position, int velocity,
 	int acceleration, int deceleration, int Direction)
 {
 	unsigned int old_epl_cnt = uiCnt_g;
-	
+
 	printf("function %s line %d\n", __FUNCTION__, __LINE__);
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0)
         return AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode;
@@ -116,7 +116,7 @@ int mc_move_absolute(int Position, int velocity,
     AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterDirection = Direction;
 
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_MoveAbsolute;
-    
+
     while((uiCnt_g - old_epl_cnt) < 200)
     {
         if((AppProcessImageOut_g.CN2_M00_MC_Status3_MovementStatusWord & MS_ABSOLUTE_BUSY) != 0)
@@ -130,31 +130,31 @@ int mc_move_absolute(int Position, int velocity,
     return ERROR_WR_PARA_TIMEOUT;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_move_additive
  *
- * description: 
- *  This function commands a controlled motion of a specific relative distance 
- *  that is added to the last commanded position. If command is executed 
- *  successfully, 0 is returned; otherwise, corresponding error number is 
- *  returned. 
+ * description:
+ *  This function commands a controlled motion of a specific relative distance
+ *  that is added to the last commanded position. If command is executed
+ *  successfully, 0 is returned; otherwise, corresponding error number is
+ *  returned.
  *
  * input:
- *  distance: Relative distance for the motion [Units]. 
+ *  distance: Relative distance for the motion [Units].
  *  velocity: Value of maximum velocity [Units/sec] (not necessarily reached).
- *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily 
- *					reached). 
- *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily 
- *					reached).   
+ *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily
+ *					reached).
+ *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily
+ *					reached).
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
-int mc_move_additive(int distance, int velocity, 
+int mc_move_additive(int distance, int velocity,
 	int acceleration, int deceleration)
 {
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0)
@@ -174,42 +174,42 @@ int mc_move_additive(int distance, int velocity,
 
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_move_velocity
  *
- * description: 
- *  This function commands an "Axis" to move a specified velocity in a specific 
- *  direction. If command is executed successfully, 0 is returned; otherwise, 
+ * description:
+ *  This function commands an "Axis" to move a specified velocity in a specific
+ *  direction. If command is executed successfully, 0 is returned; otherwise,
  *  corresponding error number is returned.
- *  
- *  To stop motion, the function block has to be interrupted by another 
+ *
+ *  To stop motion, the function block has to be interrupted by another
  *  function block issuing a new command.
  *
  * input:
  *  velocity: Value of maximum velocity [Units/sec] (not necessarily reached).
- *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily 
- *					reached). 
- *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily 
- *					reached).   
+ *  acceleration: Value of maximum acceleration [Units/sec2] (not necessarily
+ *					reached).
+ *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily
+ *					reached).
  *  Direction: Movement direction:
  *					mcPOSITIVE_DIR ... 0
  *					mcNEGATIVE_DIR ... 1
  *					mcCURRENT_DIR .... 2
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
-int mc_move_velocity(int velocity, int acceleration, 
+int mc_move_velocity(int velocity, int acceleration,
 	int deceleration, int Direction)
 {
     unsigned int old_epl_cnt = uiCnt_g;
-	
+
 	printf("function %s line %d\n", __FUNCTION__, __LINE__);
-	
+
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0)
         return AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode;
 
@@ -222,7 +222,7 @@ int mc_move_velocity(int velocity, int acceleration,
     AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterDirection = Direction;
 
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_MoveVelocity;
-    
+
     while((uiCnt_g - old_epl_cnt) < 200)
     {
         if((AppProcessImageOut_g.CN2_M00_MC_Status3_MovementStatusWord & MS_VELOCITY_BUSY) != 0)
@@ -236,28 +236,28 @@ int mc_move_velocity(int velocity, int acceleration,
 
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_set_positive_end_position
  *
- * description: 
- *  This command sets the limit position in positive direction for movement. 
- *  If command is executed successfully, 0 is returned; otherwise, 
+ * description:
+ *  This command sets the limit position in positive direction for movement.
+ *  If command is executed successfully, 0 is returned; otherwise,
  *  corresponding error number is returned.
- *  
+ *
  * input:
- *  position: Absolute limited position in positive direction for 
+ *  position: Absolute limited position in positive direction for
  *  movement [Units].
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_set_positive_end_position(int position)
 {
-    
+
     unsigned int old_epl_cnt = uiCnt_g;
 
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0)
@@ -278,26 +278,26 @@ int mc_set_positive_end_position(int position)
         usleep(5000);
     }
 
-    return ERROR_WR_PARA_TIMEOUT;	
+    return ERROR_WR_PARA_TIMEOUT;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_set_negative_end_position
  *
- * description: 
- *  This command sets the limit position in negative direction for movement. 
- *  If command is executed successfully, 0 is returned; otherwise, 
+ * description:
+ *  This command sets the limit position in negative direction for movement.
+ *  If command is executed successfully, 0 is returned; otherwise,
  *  corresponding error number is returned.
- *  
+ *
  * input:
- *  position: Absolute limited position in negative direction for 
+ *  position: Absolute limited position in negative direction for
  *  movement [Units].
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_set_negative_end_position(int position)
@@ -312,7 +312,7 @@ int mc_set_negative_end_position(int position)
 
     AppProcessImageIn_g.CN2_M80_MC_Control_ParaID = 127;
     AppProcessImageIn_g.CN2_M40_MC_Parameter_ParaValue = position;
-    
+
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_SetSWEndSwitch;
 
     while((uiCnt_g - old_epl_cnt) < 20)
@@ -322,40 +322,40 @@ int mc_set_negative_end_position(int position)
         usleep(5000);
     }
 
-    return ERROR_WR_PARA_TIMEOUT;	
+    return ERROR_WR_PARA_TIMEOUT;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_stop
  *
- * description: 
- *  This function commands a controlled motion stop and transfers the axis to 
- *  the state stopping. If command is executed successfully, 0 is returned; 
- *  otherwise, corresponding error number is returned. 
- *  
+ * description:
+ *  This function commands a controlled motion stop and transfers the axis to
+ *  the state stopping. If command is executed successfully, 0 is returned;
+ *  otherwise, corresponding error number is returned.
+ *
  * input:
- *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily 
- *					reached).   
+ *  deceleration: Value of maximum deceleration [Units/sec2] (not necessarily
+ *					reached).
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_stop(int deceleration)
 {
 	unsigned int old_epl_cnt = uiCnt_g;
-	
+
 	printf("function %s line %d\n", __FUNCTION__, __LINE__);
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0)
     {
 		printf("line %d error id %d\n", AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode);
         return AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode;
 	}
-	
-    AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterDeceleration = deceleration;    
+
+    AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterDeceleration = deceleration;
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_Stop;
 
     while((uiCnt_g - old_epl_cnt) < 200)
@@ -369,25 +369,25 @@ int mc_stop(int deceleration)
         usleep(5000);
     }
 
-    return ERROR_WR_PARA_TIMEOUT;	
+    return ERROR_WR_PARA_TIMEOUT;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_reset_error
  *
- * description: 
- *  This function resets all internal axis related errors. If command is 
- *  executed successfully, 0 is returned; otherwise, corresponding error number 
- *  is returned. 
- *  
+ * description:
+ *  This function resets all internal axis related errors. If command is
+ *  executed successfully, 0 is returned; otherwise, corresponding error number
+ *  is returned.
+ *
  * input:
  *  none
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_reset_error()
@@ -397,20 +397,20 @@ int mc_reset_error()
     return 0;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_get_position
  *
- * description: 
+ * description:
  *  This function retrieves actual position of the axis.
- *  
+ *
  * input:
  *  none
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  Position: Actual position of the axis 
+ *  Position: Actual position of the axis
  *
  ******************************************************************************/
 int mc_get_position()
@@ -418,20 +418,20 @@ int mc_get_position()
     return AppProcessImageOut_g.CN2_MC0_MC_Status2_ActualPosition;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_get_velocity
  *
- * description: 
+ * description:
  *  This function retrieves actual velocity of the axis.
- *  
+ *
  * input:
  *  none
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  Velocity: Actual velocity of the axis 
+ *  Velocity: Actual velocity of the axis
  *
  ******************************************************************************/
 int mc_get_velocity()
@@ -439,20 +439,20 @@ int mc_get_velocity()
     return AppProcessImageOut_g.CN2_MC0_MC_Status2_ActualVelocity;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_get_error_id
  *
- * description: 
+ * description:
  *  This function retrieves error number of the axis.
- *  
+ *
  * input:
  *  none
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  error_id: error number 
+ *  error_id: error number
  *
  ******************************************************************************/
 
@@ -461,34 +461,34 @@ int mc_get_error_id()
     return AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_power
  *
- * description: 
- *  This function switches on the controller of the axis. If command is executed 
- *  successfully, 0 is returned; otherwise, corresponding error number  is 
- *  returned. 
+ * description:
+ *  This function switches on the controller of the axis. If command is executed
+ *  successfully, 0 is returned; otherwise, corresponding error number  is
+ *  returned.
  *
  * input:
- *  none    
+ *  none
  *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_power()
 {
-    
+
     unsigned int old_epl_cnt;
-    
+
     old_epl_cnt = uiCnt_g;
-    
+
     if((AppProcessImageOut_g.CN2_M00_MC_Status1_AxisStatusWord & Status_Disabled) == 0)
     	return 0;
-    
+
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_Power;
 
     while((uiCnt_g - old_epl_cnt) < 20)
@@ -501,21 +501,21 @@ int mc_power()
     return ERROR_POWER_FAIL;
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_home
  *
- * description: 
- *  This command starts the homing movement of an axis according to the inputs 
- *  of the function block and the homing parameters included in the axis 
- *  structure. If command is executed successfully, 0 is returned; otherwise, 
- *  corresponding error number is returned. 
+ * description:
+ *  This command starts the homing movement of an axis according to the inputs
+ *  of the function block and the homing parameters included in the axis
+ *  structure. If command is executed successfully, 0 is returned; otherwise,
+ *  corresponding error number is returned.
  *
  * input:
  *  Position: Absolute position when the reference signal is detected [Units].
  *   Note:
- *     The position is not used with modes mcHOME_AXIS_REF and 
+ *     The position is not used with modes mcHOME_AXIS_REF and
  *     mcHOME_RESTORE_POS
- * 
+ *
  *  Direction: Movement direction:
  *  Homing Modes:
  *   	mcHOME_DEFAULT .. 0(use Parameter from Axis-INIT-Module)
@@ -524,36 +524,36 @@ int mc_power()
  *   	mcHOME_LIMIT_SWITCH .. 3
  *   	mcHOME_ABSOLUTE .. 4(offset for Absolute Encoder)
  *   	mcHOME_REF_PULSE .. 5
- *   	mcHOME_ABSOLUTE_CORR .. 6(offset for Absolute Encoder with counter 
+ *   	mcHOME_ABSOLUTE_CORR .. 6(offset for Absolute Encoder with counter
  *			range correction)
  *   	mcHOME_SET_PHASE .. 7
  *   	mcHOME_SWITCH_GATE .. 8
  *   	mcHOME_DCM .. 9(distance coded reference marks)
- *   	mcHOME_DCM_CORR .. 10 (distance coded reference marks with counting 
+ *   	mcHOME_DCM_CORR .. 10 (distance coded reference marks with counting
  *			range correction)
- *   	mcHOME_RESTORE_POS .. 11(Restore position from permanent memory, 
+ *   	mcHOME_RESTORE_POS .. 11(Restore position from permanent memory,
  *			see BR_InitEndlessPosition)
- *   	mcHOME_AXIS_REF .. 12(all parameter of the axis structure are used) 
- * 
+ *   	mcHOME_AXIS_REF .. 12(all parameter of the axis structure are used)
+ *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  errid: error number 
+ *  errid: error number
  *
  ******************************************************************************/
 int mc_home(int Position, int HomingMode)
 {
     unsigned int old_epl_cnt;
-    
+
     if((AppProcessImageOut_g.CN2_M00_MC_Status1_AxisStatusWord & Status_HomeingOK) != 0)
     	return 0;
-    	
+
     AppProcessImageIn_g.CN2_M80_MC_Control_CommandWord |= CW_Home;
 
     AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterHomePosition = Position;
     AppProcessImageIn_g.CN2_M40_MC_Parameter_ParameterHomeMode = HomingMode;
-   
+
     old_epl_cnt = uiCnt_g;
 
     while((uiCnt_g - old_epl_cnt) < 20)
@@ -570,22 +570,22 @@ int mc_home(int Position, int HomingMode)
 
 }
 /*******************************************************************************
- * function: 
+ * function:
  *  mc_init
  *
- * description: 
- *  This function initializes Ethernet Powerlink network and motor. If no 
- *  errors are present on the network, 0 is returned; otherwise, corresponding 
+ * description:
+ *  This function initializes Ethernet Powerlink network and motor. If no
+ *  errors are present on the network, 0 is returned; otherwise, corresponding
  *  error number is returned.
  *
  * input:
- *  devName: The Ethernet device name for powerlink network 
- *  
+ *  devName: The Ethernet device name for powerlink network
+ *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  ret: error number 
+ *  ret: error number
  *
  ******************************************************************************/
 int mc_init(char* devName)
@@ -593,7 +593,7 @@ int mc_init(char* devName)
 
     int ret;
     int try_cnt = 0;
-    
+
     try_cnt = 0;
     while(try_cnt < 3)
     {
@@ -662,20 +662,20 @@ int mc_init(char* devName)
 }
 
 /*******************************************************************************
- * function: 
+ * function:
  *  PrintMCStatus
  *
- * description: 
+ * description:
  *  This function prints status of movement for debugging
  *
  * input:
  *  none
- *  
+ *
  * output:
- *  none    
+ *  none
  *
  * return:
- *  none 
+ *  none
  *
  ******************************************************************************/
 
@@ -762,7 +762,7 @@ tEplKernel PUBLIC AppCbSync(void)
     uiCnt_g++;
 
     if(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode != 0 && err_output_switch)
-    {   
+    {
 
         printf("actual statusword is 0x%04x\n", AppProcessImageOut_g.CN2_M00_MC_Status1_AxisStatusWord);
         get_err_str(AppProcessImageOut_g.CN2_M00_MC_Status1_ErrorCode, err_string);
